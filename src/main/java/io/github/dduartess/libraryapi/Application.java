947a0d177d67d7a99e8f551fb9ef88f6e39a5c36
dduartess <dandandsoares@gmail.com>
2025-11-10 15:10:32 -0300

    Create test for persist API

---
+++ src/main/java/io/github/dduartess/libraryapi/Application.java
@@ -0,0 +1,29 @@
+package io.github.dduartess.libraryapi;
+
+import io.github.dduartess.libraryapi.model.Autor;
+import io.github.dduartess.libraryapi.repository.AutorRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		var context = SpringApplication.run(Application.class, args);
+        AutorRepository repository = context.getBean(AutorRepository.class);
+
+        exemploSalvarRegistro(repository);
+	}
+
+    public static void exemploSalvarRegistro(AutorRepository autorRepository){
+        Autor autor = new Autor();
+        autor.setNome("José");
+        autor.setNacionalidade("Brasileira");
+        autor.setDataNascimento(LocalDate.of(1950,1,31));
+
+        var autorSalvo = autorRepository.save(autor);
+        System.out.println("Autor salvo " + autorSalvo);
+    }
+}
